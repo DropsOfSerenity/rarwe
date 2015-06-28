016dd20fe54c7aa981e196fcb27970e355086ee8
Justin Schiff <AmbientRevolution@gmail.com>
2015-06-28 11:58:51 -0700

    song creation.

---
+++ app/controllers/band/songs.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  title: '',
+  songCreationStarted: false,
+
+  isAddButtonDisabled: function() {
+    return Ember.isEmpty(this.get('title'));
+  }.property('title'),
+
+  noSongs: function() {
+    return this.get('model.length') === 0;
+  }.property('model.length'),
+
+  canCreateSong: function() {
+    return this.get('songCreationStarted') || this.get('model.length')
+  }.property('songCreationStarted', 'model.length'),
+
+  actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true)
+    }
+  }
+
+});
